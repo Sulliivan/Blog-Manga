4b7907cc4f8067716172ecfdb83a1ae93a38f2ec
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-12-09 08:33:08 +0100

    Put and delete

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -58,4 +58,55 @@ router.post('/admin', async (req, res) => {
     }
 });
 
+/////////////  Pour supprimer un Article   //////////////
+
+router.delete('/admin/:postId', async (req, res) => {
+    try {
+        const id = req.params.postId
+        const articles = await query("DELETE FROM post WHERE postId = '" + id + "';")
+            res.redirect("/dashboard/admin")
+    } catch (err) {
+        res.send(err)
+    }
+});
+
+
+/////////////  Pour Modifier un Article   //////////////
+
+router.put('/admin/:postId', async(req, res) => {
+    let id = req.params.postId
+    let title = req.body.title
+    let content = req.body.content
+    let description = req.body.description
+    let dateCreated = req.body.dateCreated
+    let status = req.body.status
+    let user = req.body.userId
+    let Category = req.body.CategoryId
+    let imageUpload = req.files.image
+    let imageBack = req.files.backImage
+    let images = `/images/${imageUpload.name}`
+    let img = `/images/${imageBack.name}`
+
+    if (imageUpload.mimetype === "image/jpeg" || imageUpload.mimetype === "image/jpg" || imageUpload.mimetype === "image/gif" || imageUpload.mimetype === "image/png") {
+        imageUpload.mv(`public/images/${imageUpload.name}`, async function (err) {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            if (imageBack.mimetype === "image/jpeg" || imageBack.mimetype === "image/jpg" || imageBack.mimetype === "image/gif" || imageBack.mimetype === "image/png") {
+                imageBack.mv(`public/images/${imageBack.name}`, async function (err) {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    try {
+                        await query("UPDATE post SET title = '"+ title +"', content = '"+ content +"', description = '"+ description +"', status = '"+ status +"', image = '"+ images +"', backImage = '"+ img +"', userId = '"+ user +"', CategoryId = '"+ Category +"', dateCreated = '"+ dateCreated +"' WHERE postId = '" + id +"' ");
+                        res.redirect("/dashboard/admin")
+                    } catch (err) {
+                        res.send(err)
+                    } ;
+                });
+            }
+        });
+    }
+});
+
 module.exports = router;
