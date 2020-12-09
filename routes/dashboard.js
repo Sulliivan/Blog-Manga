49a9cd2183484b43a4790e099f7d78a4caba9188
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-12-09 08:32:15 +0100

    Post an article

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -18,4 +18,44 @@ router.get('/admin', async (req, res) => {
         
 });
 
+
+/////////////  Pour Poster un Article   //////////////
+router.post('/admin', async (req, res) => {
+
+    let title = req.body.title
+    let content = req.body.content
+    let description = req.body.description
+    let dateCreated = req.body.dateCreated
+    let status = req.body.status
+    let user = req.body.userId
+    let Category = req.body.CategoryId
+    let imageUpload = req.files.image
+    let imageBack = req.files.backImage
+    let images = `/images/${imageUpload.name}`
+    let img = `/images/${imageBack.name}`
+
+    if (imageUpload.mimetype === "image/jpeg" || imageUpload.mimetype === "image/jpg" || imageUpload.mimetype === "image/gif" || imageUpload.mimetype === "image/png") {
+        imageUpload.mv(`public/images/${imageUpload.name}`, async function (err) {
+            if (err) {
+                return res.status(500).send(err)
+            }
+           
+            if (imageBack.mimetype === "image/jpeg" || imageBack.mimetype === "image/jpg" || imageBack.mimetype === "image/gif" || imageBack.mimetype === "image/png") {
+                imageBack.mv(`public/images/${imageBack.name}`, async function (err) {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    
+                    try {
+                        await query("insert into post (title, content, description, status, image, backImage, userId, CategoryId, dateCreated) values (?, ?, ?, ?, ?, ?, ?, ?, NOW() );", [title, content, description, status, images, img, user, Category, dateCreated])
+                            res.redirect("/dashboard/admin")
+                    } catch (err) {
+                        res.send(err)
+                    } 
+                });
+            }
+        });
+    }
+});
+
 module.exports = router;
