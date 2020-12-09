8d43b28a2d086f3b57395a547a595b81b386ccb1
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-12-09 08:31:22 +0100

    Get an article

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,21 @@
-const express = require("express")
-,     router = express.Router()
-,     indexController = require('../controllers/index.controller');
+const express = require('express');
+const router = express.Router();
 
-router.get('/admin' );
+
+//////////////   DASHBOARD ARTICLE   //////////////
+//// Affiche les articles dans Dashboard Article Admin //////
+router.get('/admin', async (req, res) => {
+    try {
+        const id = req.params.postId
+        // const articles = await query("SELECT postId, title, description, content,  image, dateCreated, status, userId, CategoryId FROM post WHERE postId =  '" + id + "';")
+        const articles = await query("SELECT postId, title, description, content, image, dateCreated, status, userId, CategoryId FROM post ")
+        res.render("admin" , {
+            articles
+        })
+    }catch(err){
+        res.send(err)
+    }
+        
+});
 
 module.exports = router;
